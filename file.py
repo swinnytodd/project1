b04b4afd014d592bb0a422b07d5e00524c7b9103
Taylor Gang <swinnytodd225@gmail.com>
2022-03-19 15:02:52 +0300

    Just fix it

--- file.py
+++ file.py
@@ -1,4 +1,19 @@
-a = 4
-b = 5
-c = 4 + 5
-print(c)
+. . .
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
